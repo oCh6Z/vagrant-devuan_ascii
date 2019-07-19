49b315f50a9e64ba73e33e900e794d5d385fb523
och6z <44880194+och6z@users.noreply.github.com>
2019-07-19 13:32:28 +0200

    Update pyenv.sh

--- scripts/pyenv.sh
+++ scripts/pyenv.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 installPyenv () {
     apt -y -qq install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libterm-readline-gnu-perl libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python-openssl git 2>/dev/null
     apt -y -qq install --no-install-recommends libedit-dev 2>/dev/null
