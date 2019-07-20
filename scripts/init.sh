8f1806323ab3ba2dd0ca793d8de3acabb83c17b1
och6z <44880194+och6z@users.noreply.github.com>
2019-07-20 12:25:45 +0200

    Reformat upgradeBase, installVBGuest

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,13 +2,13 @@
 
 upgradeBase () {
     export DEBIAN_FRONTEND=noninteractive
-    apt -y -qq update 2>/dev/null
+    apt update -y -qq > /dev/null 2>&1
     apt-mark hold grub-pc openssh-server
-    apt -y -qq upgrade 2>/dev/null
+    apt upgrade -y -qq > /dev/null 2>&1
 }
 
 installVBGuest () {
-    apt -y -qq install --no-install-recommends linux-headers-$(uname -r) build-essential dkms 2>/dev/null
+    apt install -y -qq --no-install-recommends linux-headers-$(uname -r) build-essential dkms > /dev/null 2>&1
 }
 
 funcs=(upgradeBase installVBGuest)
