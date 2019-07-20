aafd4a775d49a620187b7036c002659ecfe10b99
och6z <44880194+och6z@users.noreply.github.com>
2019-07-20 12:19:45 +0200

    Reformat installPyenv

--- scripts/pyenv.sh
+++ scripts/pyenv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 installPyenv () {
-    apt -y -qq install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libterm-readline-gnu-perl libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python-openssl git 2>/dev/null
-    apt -y -qq install --no-install-recommends libedit-dev 2>/dev/null
-    apt -y -qq install --no-install-recommends libssl-dev 2>/dev/null
-    apt -y -qq install --no-install-recommends libssl1.0-dev 2>/dev/null
-    curl --silent https://pyenv.run bash | bash 2>/dev/null
+    apt install -y -qq --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libterm-readline-gnu-perl libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python-openssl git > /dev/null 2>&1
+    apt install -y -qq --no-install-recommends libedit-dev > /dev/null 2>&1
+    apt install -y -qq --no-install-recommends libssl-dev > /dev/null 2>&1
+    apt install -y -qq --no-install-recommends libssl1.0-dev > /dev/null 2>&1
+    curl --silent https://pyenv.run bash | bash > /dev/null 2>&1
     echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
     echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
